329b123dee5fc5ed3a915bbe56ea4d24041b6a41
Emanoel91 <121406187+Emanoel91@users.noreply.github.com>
2022-12-27 16:07:51 +0330

    Update File.py

--- File.py
+++ File.py
@@ -3,8 +3,8 @@ import streamlit as st
 from PIL import Image
 
 # Layout
-st.set_page_config(page_title='Dashboard Name', page_icon=':bar_chart:', layout='wide')
-st.title('Dashboard Name')
+st.set_page_config(page_title='Near Megadashboard', page_icon=':bar_chart:', layout='wide')
+st.title('Near Megadashboard')
 
 # Content
 c1, c2, c3 = st.columns(3)
