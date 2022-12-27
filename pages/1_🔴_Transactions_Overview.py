879af09a951988e0418fa841bdafe3021ca696d0
Emanoel91 <121406187+Emanoel91@users.noreply.github.com>
2022-12-27 17:31:57 +0330

    Update 1_🔴_Transactions_Overview.py

--- pages/1_🔴_Transactions_Overview.py
+++ pages/1_🔴_Transactions_Overview.py
@@ -24,7 +24,8 @@ def get_data(query1):
 transactions_overview = get_data('Trnsactions Overview')
 
 # Single chain Analysis
-
+subtab_overview, subtab_shares, subtab_amount = st.tabs(['Overview', 'Market Shares', 'Swap Amount'])
+with subtab_overview:
 c1, c2 = st.columns(2)
     
 with c1:
