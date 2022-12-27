52f3526286e55f1212c9595f75a308bf1ce8899c
Emanoel91 <121406187+Emanoel91@users.noreply.github.com>
2022-12-27 19:01:51 +0330

    Update 1_🔴_Transactions_Overview.py

--- pages/1_🔴_Transactions_Overview.py
+++ pages/1_🔴_Transactions_Overview.py
@@ -24,6 +24,7 @@ def get_data(query1):
 transactions_overview = get_data('Trnsactions Overview')
 
 # Single chain Analysis
+st.subheader('Overview')
 df = transactions_overview
 c1, c2 = st.columns(2)
     
