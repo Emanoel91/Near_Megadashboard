1a5d257557834f465718b803a116be89eb52b3d5
Emanoel91 <121406187+Emanoel91@users.noreply.github.com>
2022-12-27 19:17:21 +0330

    Update 1_🔴_Transactions_Overview.py

--- pages/1_🔴_Transactions_Overview.py
+++ pages/1_🔴_Transactions_Overview.py
@@ -29,11 +29,11 @@ df = transactions_overview
 c1, c2 = st.columns(2)
     
 with c1:
-        st.metric(label='Blocks Count', value=df['Blocks Count'])
-        st.metric(label='Transactions Count', value=df['Transactions Count'].round(2))
+        st.metric(label='Total Blocks Count', value=df['Total Blocks Count'])
+        st.metric(label='Total Transactions Count', value=df['Total Transactions Count'].round(2))
         st.metric(label='Total Transactions Count per Receiver', value=df['Total Transactions Count per Receiver'].round(3))
 with c2:
-        st.metric(label='Tx Receivers Count', value=df['Tx Receivers Count'])
-        st.metric(label='Tx Senders Count', value=df['Tx Senders Count'].round(2))
+        st.metric(label='Total Tx Receivers Count', value=df['Total Tx Receivers Count'])
+        st.metric(label='Total Tx Senders Count', value=df['Total Tx Senders Count'].round(2))
         st.metric(label='Total Transactions Count per Sender', value=df['Total Transactions Count per Sender'].round(3))
     
