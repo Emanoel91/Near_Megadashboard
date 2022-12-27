ce4cd391e5b2e998be1689a2a4f3938d00da1dbd
Emanoel91 <121406187+Emanoel91@users.noreply.github.com>
2022-12-28 02:16:05 +0330

    Update 1_🔴_Transactions_Overview.py

--- pages/1_🔴_Transactions_Overview.py
+++ pages/1_🔴_Transactions_Overview.py
@@ -13,10 +13,8 @@ st.set_page_config(page_title='Transactions Overview - Near Megadashboard', page
 st.title('🔴 Transactions Overview')
 
 # Cover
-c1, c2 = st.columns(2)
 
-c1.image(Image.open('Images/near4-logo.JPG'))
-c2.image(Image.open('Images/chain1-logo.JPG'))
+image(Image.open('Images/chain1-logo.JPG'))
 
 # Style
 with open('style.css')as f:
