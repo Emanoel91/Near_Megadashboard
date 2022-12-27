eaaebc2438a5dc63f04dfdcf78d9a35d18192745
Emanoel91 <121406187+Emanoel91@users.noreply.github.com>
2022-12-27 23:00:21 +0330

    Update 1_🔴_Transactions_Overview.py

--- pages/1_🔴_Transactions_Overview.py
+++ pages/1_🔴_Transactions_Overview.py
@@ -29,11 +29,15 @@ df = transactions_overview
 c1, c2 = st.columns(2)
     
 with c1:
-        st.metric(label='Total Blocks Count', value=df['Total Blocks Count'])
-        st.metric(label='Total Transactions Count', value=df['Total Transactions Count'].round(2))
-        st.metric(label='Average Transactions Count per Receiver', value=df['Average Transactions Count per Receiver'].round(3))
+        st.metric(label='Total Transactions Count', value=df['Total Transactions Count'])
+        st.metric(label='Successful Transactions', value=df['Successful Transactions'].round(2))
+        st.metric(label='Total Blocks Count', value=df['Total Blocks Count'].round(3))
+        st.metric(label='Total Tx Senders Count', value=df['Total Tx Senders Count'].round(4))
+        st.metric(label='Average Transactions Count per Receiver', value=df['Average Transactions Count per Receiver'].round(5))
 with c2:
-        st.metric(label='Total Tx Receivers Count', value=df['Total Tx Receivers Count'])
-        st.metric(label='Total Tx Senders Count', value=df['Total Tx Senders Count'].round(2))
-        st.metric(label='Average Transactions Count per Sender', value=df['Average Transactions Count per Sender'].round(3))
+        st.metric(label='Average Success Rate', value=df['Average Success Rate'])
+        st.metric(label='Failed Transactions', value=df['Failed Transactions'].round(2))
+        st.metric(label='Average Transaction Count per Block', value=df['Average Transaction Count per Block'].round(3))
+        st.metric(label='Total Tx Receivers Count', value=df['Total Tx Receivers Count'].round(4))
+        st.metric(label='Average Transactions Count per Receiver', value=df['Average Transactions Count per Receiver'].round(5))
     
