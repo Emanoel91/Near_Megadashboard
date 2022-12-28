87096644113ee7dcc277d1ae4a5a7e776daf6545
Emanoel91 <121406187+Emanoel91@users.noreply.github.com>
2022-12-28 22:02:34 +0330

    Update 1_🔴_Transactions_Overview.py

--- pages/1_🔴_Transactions_Overview.py
+++ pages/1_🔴_Transactions_Overview.py
@@ -65,6 +65,10 @@ fig = px.area(df, x='Date', y='Blocks Count', title='Daily Blocks Count')
 fig.update_layout(legend_title=None, xaxis_title=None, yaxis_title='Blocks Count')
 st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
 
+fig = px.bar(df, x='Date', y='Average Transaction Count per Block', color='', title='Average Transaction Count per Block', log_y=True)
+fig.update_layout(showlegend=False, xaxis_title=None, yaxis_title='TX per Block', xaxis={'categoryorder':'total ascending'})
+st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
+
 fig = sp.make_subplots(specs=[[{'secondary_y': True}]])
 fig.add_trace(go.Bar(x=df['Date'], y=df['Average Transactions Count per Sender'], name='TX per Sender'), secondary_y=False)
 fig.add_trace(go.Line(x=df['Date'], y=df['Average Transactions Count per Receiver'], name='TX per Receiver'), secondary_y=True)
