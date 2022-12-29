308ac36b186b9e9d9579e83f76c2c8fd16c8135a
Emanoel91 <121406187+Emanoel91@users.noreply.github.com>
2022-12-29 04:29:04 +0330

    Update 2_🟠_Price.py

--- pages/2_🟠_Price.py
+++ pages/2_🟠_Price.py
@@ -33,7 +33,7 @@ NEAR_Price_per_Day = get_data('NEAR Price per Day')
 
 df = NEAR_Price_per_Day
         
-fig = px.line(df, x='Date', y='Price', color='CRITERIA', title='NEAR Price per Day', log_y=True)
+fig = px.area(df, x='Date', y='Price', color='CRITERIA', title='NEAR Price per Day', log_y=False)
 fig.update_layout(showlegend=False, xaxis_title=None, yaxis_title='Price')
 st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
 
