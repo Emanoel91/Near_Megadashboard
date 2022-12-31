fcb78a33ae15c45d9fd61443bb9e19037dee2596
Emanoel91 <121406187+Emanoel91@users.noreply.github.com>
2022-12-31 20:17:23 +0330

    Update 3_🟡_Transfers.py

--- pages/3_🟡_Transfers.py
+++ pages/3_🟡_Transfers.py
@@ -70,11 +70,11 @@ st.subheader('2️⃣ Daily Transfers')
 df = Transfers
 
 fig = px.bar(df, x='Date', y='Transfers Volume', color='STATUS', title='Daily Transfers Volume', log_y=False)
-fig.update_layout(showlegend=False, xaxis_title=None, legend_title='STATUS', yaxis_title='$NEAR', xaxis={'categoryorder':'total ascending'})
+fig.update_layout(showlegend=False, xaxis_title=None, legend_title='STATUS', yaxis_title='$NEAR (🔴Failed 🟢Successful)', xaxis={'categoryorder':'total ascending'})
 st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
 
 fig = px.bar(df, x='Date', y='Transfers Count', color='STATUS', title='Daily Transfers Count', log_y=False)
-fig.update_layout(showlegend=False, xaxis_title=None, legend_title='STATUS', yaxis_title='', xaxis={'categoryorder':'total ascending'})
+fig.update_layout(showlegend=False, xaxis_title=None, legend_title='STATUS', yaxis_title='🔴Failed 🟢Successful', xaxis={'categoryorder':'total ascending'})
 st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
 
 
