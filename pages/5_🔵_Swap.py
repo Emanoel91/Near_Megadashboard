0710cc65ee5f3dc5397d39be47ffe7ab45b025e6
Emanoel91 <121406187+Emanoel91@users.noreply.github.com>
2023-01-01 20:09:11 +0330

    Update 5_🔵_Swap.py

--- pages/5_🔵_Swap.py
+++ pages/5_🔵_Swap.py
@@ -216,13 +216,6 @@ fig = px.bar(df, x='Token Out', y='Swaps Volume', color='Token Out', title='Dist
 fig.update_layout(showlegend=False, xaxis_title=None, legend_title='Token Out', yaxis_title='Volume($USD)', xaxis={'categoryorder':'total ascending'})
 st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
 
-df = Tokens
-fig = sp.make_subplots(specs=[[{'secondary_y': True}]])
-fig.add_trace(go.Bar(x=df['Token'], y=df['Buying Volume'], name='Total Buying Volume'), secondary_y=False)
-fig.add_trace(go.Line(x=df['Token'], y=df['Selling Volume'], name='Total Selling Volume'), secondary_y=True)
-fig.update_layout(title_text='Swaps Volume By Volume')
-fig.update_yaxes(title_text='$USD', secondary_y=False)
-fig.update_yaxes(title_text='$USD', secondary_y=True)
-st.plotly_chart(fig, use_container_width=True, theme=theme_plotly)
+
 
               
