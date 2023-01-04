c2fbcc63b54028902a10bb4010895bec556067d7
Emanoel91 <121406187+Emanoel91@users.noreply.github.com>
2023-01-05 00:22:13 +0330

    Update 7_🟤_NFTs.py

--- pages/7_🟤_NFTs.py
+++ pages/7_🟤_NFTs.py
@@ -9,7 +9,7 @@ from PIL import Image
 theme_plotly = None # None or streamlit
 
 # Structure
-st.set_page_config(page_title='NFTs - Near Megadashboard', page_icon=':brown_circle:', layout='wide')
+st.set_page_config(page_title='NFTs - Near Megadashboard', page_icon=':chart_with_upwards_trend:', layout='wide')
 st.title('🟤 NFTs')
 
 # Cover
