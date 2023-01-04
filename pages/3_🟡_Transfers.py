b8914c72cc4f03e9baa09bb82983f063dbc9cd21
Emanoel91 <121406187+Emanoel91@users.noreply.github.com>
2023-01-05 00:15:38 +0330

    Update 3_🟡_Transfers.py

--- pages/3_🟡_Transfers.py
+++ pages/3_🟡_Transfers.py
@@ -9,7 +9,7 @@ from PIL import Image
 theme_plotly = None # None or streamlit
 
 # Structure
-st.set_page_config(page_title='Transfers - Near Megadashboard', page_icon=':bar_chart:', layout='wide')
+st.set_page_config(page_title='Transfers - Near Megadashboard', page_icon=':yellow_circle:', layout='wide')
 st.title('🟡 Transfers')
 
 # Cover
