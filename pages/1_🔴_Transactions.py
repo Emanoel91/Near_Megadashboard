41f7918ddc9cc532061300953f59fa84ca132a7a
Emanoel91 <121406187+Emanoel91@users.noreply.github.com>
2023-01-07 09:45:49 +0330

    Update 1_🔴_Transactions.py

--- pages/1_🔴_Transactions.py
+++ pages/1_🔴_Transactions.py
@@ -20,10 +20,13 @@ c1.image(Image.open('Images/transactions.JPG'))
 with c2: 
         st.write(
             """
-            1️⃣ ###### Overview
-            2️⃣ ###### Daily Transactions
-            3️⃣ ###### Activity of Addresses
-            4️⃣ ###### Transaction Fees
+            1️⃣ ######Overview
+            
+            2️⃣ #### Daily Transactions
+            
+            3️⃣ #### Activity of Addresses
+            
+            4️⃣ #### Transaction Fees
             """
             )
 
